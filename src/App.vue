3b81f5671a4fda0c6a7cdd49485aabf253a521c0
Matis <matisleima@gmail.com>
2023-09-07 21:19:59 +0300

    N viimane

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-<!--  <nav>-->
-<!--    <router-link to="/">Home</router-link> |-->
-<!--    <router-link to="/about">About</router-link>-->
-<!--  </nav>-->
+  <!--  <nav>-->
+  <!--    <router-link to="/">Home</router-link> |-->
+  <!--    <router-link to="/about">About</router-link>-->
+  <!--  </nav>-->
 
-  <router-view/>
+  <!--  <router-view/>-->
   <login-view/>
-
 
 
 </template>
---
+++ src/views/LandingView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="background-color: brown" class="home">
+
+<h1>LANDING PAGE</h1>
+
+    <img src="" alt="">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'LandingView',
+
+}
+</script>
