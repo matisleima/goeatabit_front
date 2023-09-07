d55776fb18b078ecb5a6aaf9b9ab14dbd998ee35
Matis <matisleima@gmail.com>
2023-09-07 15:23:52 +0300

    Logini ehitamine

---
+++ src/views/LoginView.vue
@@ -0,0 +1,77 @@
+<template>
+
+  <div class="container">
+    <div class="row">
+      <h1>TÜHJUS</h1>
+    </div>
+    <div class="row">tühjus</div>
+    <div class="row">tühjus</div>
+
+    <div class="row">
+      <div class="col">
+
+      </div>
+
+
+      <div class="col">
+
+
+      </div>
+
+      <div class="col">
+        <form>
+          <div class="d-grid gap-3">
+            <input v-model="email" type="text" class="form-control" id="exampleInputEmail1"
+                   placeholder="Sisesta email">
+            <input v-model="password" type="password" class="form-control" id="exampleInputPassword1"
+                   placeholder="Sisesta parool">
+            <button @click="login" type="submit" class="btn btn-primary">Logi sisse</button>
+            <button @click="login" type="submit" class="btn btn-primary">Loo kasutaja</button>
+          </div>
+        </form>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      loginResponse: {
+        userId: 0
+      }
+    }
+  },
+
+  methods: {
+    login() {
+      this.$http.get("/login", {
+            params: {
+              email: this.email,
+              password: this.password
+            }
+          }
+      ).then(response => {
+        // Siit saame kätte JSONi  ↓↓↓↓↓↓↓↓
+        this.loginResponse.userId = response.data.userId
+        sessionStorage.setItem('userId', this.loginResponse.userId)
+        this.goToHome()
+      }).catch(error => {
+        // Siit saame kätte errori JSONi  ↓↓↓↓↓↓↓↓
+        const errorResponseBody = error.response.data
+      })
+    },
+    goToHome() {
+      router.push({name: 'homeRoute'})
+    },
+  },
+}
+
+</script>
