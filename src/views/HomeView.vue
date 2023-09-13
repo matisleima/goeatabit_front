102245f91b10c4e5d6b549a6607184030fc45aa0
Teele Hõrandi <teele.horandi@gmail.com>
2023-09-13 09:26:53 +0300

    landingPage v1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,63 @@
 <template>
   <LogoutModal ref="logoutModalRef"/>
   <div>
-    <h1>Home PAGE</h1>
+    <h1>Tänased TOP pakkumised MINGI_FILTER</h1>
   </div>
+
+<!--  //PILDID-->
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src="../assets/script/meal.jpg" height="400" width="400"/>
+      </div>
+      <div class="col">
+       DEFAULT PILT VÕI OFFER PILT
+      </div>
+      <div class="col">
+        DEFAULT PILT VÕI OFFER PILT
+      </div>
+    </div>
+  </div>
+
+<!--  //TABEL-->
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+
+        <table class="table">
+          <thead>
+          <tr>
+            <th scope="col">Pakkuja</th>
+            <th scope="col">Pakkumine</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+          <tr>
+            <td>Jacob</td>
+            <td>Thornton</td>
+          </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+
+<!--  //NUPUD-->
 
   <div class="container text-center">
     <div class="row">
       <div class="col">
-        Column
+        <button @click="handleLogout" type="button" class="btn btn-secondary" >Logi välja</button>
       </div>
       <div class="col">
-        Column
+        <button @click="handleLogout" type="button" class="btn btn-secondary" >Logi välja</button>
       </div>
       <div class="col">
         <button @click="handleLogout" type="button" class="btn btn-secondary" >Logi välja</button>
